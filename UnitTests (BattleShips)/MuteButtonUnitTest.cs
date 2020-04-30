4c983bd108fdcc380a85a12d98d010b5498089d4
PandaPlaysAll <josh.piper@live.com.au>
2020-04-30 20:06:48 +1000

    Added Unit Test layout
    Added Unit Test layout

---
+++ UnitTests (BattleShips)/MuteButtonUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Battleships;
+
+namespace MuteButtonUnitTest
+{
+    [TestFixture]
+
+
+    public class InventoryTests
+    {
+        //declaration
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void TestHere()
+        {
+
+        }
+    }
+}
